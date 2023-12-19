67116e1f2ab56f899b309b526b03a733b898deb5
Nathan Mota <nathansmota@gmail.com>
2023-12-19 12:18:22 -0300

    :sparkles: feat: login page structure

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,6 +1,9 @@
 //import React from 'react';
 import './Login.css';
 import { Link } from 'react-router-dom';
+import { FaUserShield } from 'react-icons/fa';
+import { BsShieldLockFill } from 'react-icons/bs';
+import { AiOutlineSwapRight } from 'react-icons/ai';
 import video from '../../loginAssets/video.mp4';
 import logo from '../../loginAssets/logo.png';
 
@@ -33,9 +36,32 @@ const Login = () => {
 
                     <form action="" className='form grid'>
                         <span>Login Status will go here</span>
+
                         <div className="inputDiv">
-                            <label htmlFor="username">Username</label>                        
+                            <label htmlFor="username">Usuário</label>
+                            <div className="input flex">
+                                <FaUserShield className='icon' />
+                                <input type="text" id='username' placeholder='Entre com seu usuário' />
+                            </div>
                         </div>
+
+                        <div className="inputDiv">
+                            <label htmlFor="password">Senha</label>
+                            <div className="input flex">
+                                <BsShieldLockFill className='icon' />
+                                <input type="text" id='password' placeholder='Entre com sua senha' />
+                            </div>
+                        </div>
+
+                        <button type='submit' className='btn flex'>
+                            <span>Login</span>
+                            <AiOutlineSwapRight className='icon' />
+                        </button>
+
+                        <span className='forgotPassword'>
+                            Esqueceu sua senha? <a href="#">Clique aqui</a>
+                        </span>
+
                     </form>
                 </div>
             </div>
